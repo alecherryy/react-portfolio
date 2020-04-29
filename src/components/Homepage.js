5f2e9a6462076fd63f1ae496b640ce80c78a0cae
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2020-04-29 11:35:13 -0400

    added colors, fonts, typography and variables

---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.css'; // remove this when creating a new project
+import './App.scss';
+
+class Section extends Component {
+  render() {
+    return (
+      <div className="section">
+        <h2>Hello</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
