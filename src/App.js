84dc1cdedde1a70d267c2e7855367114df066dfd
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2020-04-29 12:36:42 -0400

    hamburger menu icon

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './index.css'; // remove this when creating a new project
+import HamburgerMenu from './components/HamburgerMenu/index';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>This is my React starter kit. It includes a basic App set up and Sass; plus a ready-to-go Modularscale.</p>
-        </header>
-      </div>
+      <HamburgerMenu />
     );
   }
 }
