ddb026ad8dca0782cfe897a149aef583fbcf21cd
Alessia Pizzoccheri <pizzoccheri.alessia5@gmail.com>
2020-10-05 01:40:06 -0400

    create build folder

---
+++ build/precache-manifest.ed7e0390c6cd8bcbac07da1d0d46ad47.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f026b410f9c1ee6de09",
+    "url": "/static/css/main.049dfe7e.chunk.css"
+  },
+  {
+    "revision": "4f026b410f9c1ee6de09",
+    "url": "/static/js/main.4f026b41.chunk.js"
+  },
+  {
+    "revision": "9ef7d3ef096c4c453b30",
+    "url": "/static/js/1.9ef7d3ef.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "467918ba78f565ce550182abfa2affc0",
+    "url": "/static/media/semi-circle-magenta-2.467918ba.svg"
+  },
+  {
+    "revision": "aa36427c69c69bd27dca28b829f4d01d",
+    "url": "/static/media/semi-circle-magenta.aa36427c.svg"
+  },
+  {
+    "revision": "b1688d7bb905f08e46b0439f22bd0aeb",
+    "url": "/static/media/semi-circle-purple.b1688d7b.svg"
+  },
+  {
+    "revision": "713a0e93152ae23575429bf5433423fd",
+    "url": "/static/media/semi-circle-purple-dark.713a0e93.svg"
+  },
+  {
+    "revision": "41960b856df1256ab0fa6324bbb1c893",
+    "url": "/static/media/semi-circle-orange-light.41960b85.svg"
+  },
+  {
+    "revision": "01be300ca33681752986840eb170f00f",
+    "url": "/static/media/semi-circle-orange.01be300c.svg"
+  },
+  {
+    "revision": "d780db3d91c2d6db7058722544ae7e7a",
+    "url": "/static/media/arrow-white-right.d780db3d.svg"
+  },
+  {
+    "revision": "f44abca74e5b2ef0258f216e49054026",
+    "url": "/static/media/github.f44abca7.svg"
+  },
+  {
+    "revision": "572fe473f12f5c3357e9b4b3d9383bd2",
+    "url": "/static/media/github-black.572fe473.svg"
+  },
+  {
+    "revision": "8c89962d4c07a097db8c4073077ff37b",
+    "url": "/static/media/github-hover.8c89962d.svg"
+  },
+  {
+    "revision": "e51e83a7b9a01934be6a0aeaeadb4dba",
+    "url": "/static/media/codepen.e51e83a7.svg"
+  },
+  {
+    "revision": "f25f912dceab4bf50d3dd4b7b70a619c",
+    "url": "/static/media/codepen-black.f25f912d.svg"
+  },
+  {
+    "revision": "23186eac899f47524dd73a72f2906d13",
+    "url": "/static/media/codepen-hover.23186eac.svg"
+  },
+  {
+    "revision": "6b351948871ebd5e9d0129da4587fe63",
+    "url": "/static/media/linkedin.6b351948.svg"
+  },
+  {
+    "revision": "5d7d6ed3cd994dbbc1d863961643bc3c",
+    "url": "/static/media/linkedin-black.5d7d6ed3.svg"
+  },
+  {
+    "revision": "2569505e445196b6a10a024200128e56",
+    "url": "/static/media/linkedin-hover.2569505e.svg"
+  },
+  {
+    "revision": "dc21091a97a27b1b3ac36d42f2c64284",
+    "url": "/static/media/twitter.dc21091a.svg"
+  },
+  {
+    "revision": "452dd408519634161edaf44707b1dedd",
+    "url": "/static/media/twitter-black.452dd408.svg"
+  },
+  {
+    "revision": "99292f08612dc68a2f427dc2dca4a668",
+    "url": "/static/media/twitter-hover.99292f08.svg"
+  },
+  {
+    "revision": "82fff96732294aca4506e284691d09e6",
+    "url": "/static/media/arrow-black-right.82fff967.svg"
+  },
+  {
+    "revision": "93630ef3404cb77de3b176371cce0dc8",
+    "url": "/index.html"
+  }
+];
